0e0d979ab259cca56b85604d89b06541f06b9d0f
Mahdi Heydari <heydarimahdi248@gmail.com>
2026-08-12 00:08:08 +0330

    Create QuizMaster.py

---
+++ QuizMaster.py
@@ -0,0 +1,13 @@
+try:
+    Number=input("Which number is \"fouteen\" ? \n A)14 \n B)4 \n C)40 \n D)44 \n :")
+    if(Number=="A" or Number=="a"):
+        print("🤩🤩 Perfect 🤩🤩")
+    elif(Number=="B" or Number=="b"):
+        print("🧐🧐 Try again 🧐🧐")
+    elif(Number=="C" or Number=="c"):
+            print("🧐🧐 Try again 🧐🧐")
+    elif(Number=="D" or Number=="d"):
+            print("🧐🧐 Try again 🧐🧐")
+    
+except:
+    print("Please select the answer from the available options!!")
